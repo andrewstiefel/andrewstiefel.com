5cd3d4c20ce3ad197488bf3ac6f214ae03df4032
Andrew S <andrew@andrewstiefel.com>
2020-05-18 18:30:43 -0700

    Create POST request

--- functions/convertkit-subscribe/convertkit-subscribe.js
+++ functions/convertkit-subscribe/convertkit-subscribe.js
@@ -1,11 +1,22 @@
+const request = require("request");
+
 exports.handler = async (event, context) => {
 
   const email = event.queryStringParameters.email || 'Oops, no email';
+  const data = {
+    api_key: 'hQIOi5G6xVzZBQ0hRZTfKg',
+    email: '${email}',
+  };
+
+  const subscriber = JSON.stringify(data);
+  console.log("Sending data to convertkit", subscriber);
 
   // Subscribe an email
 
-  return {
-    statusCode: 200,
-    body: `Received ${email}`
+  request({
+    method: "POST",
+    url: 'https://api.convertkit.com/v3/forms/d9d0c34d5f/subscribe',
+    body: subscriber,
+    headers: 'Content-Type': 'application/json; charset=utf-8',
   };
 };
