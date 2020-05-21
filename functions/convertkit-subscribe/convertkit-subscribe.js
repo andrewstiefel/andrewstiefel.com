3b107c2154d2a36ce2ab9f69d79fc3fcaa8b18d1
Andrew S <andrew@andrewstiefel.com>
2020-05-20 20:55:24 -0700

    Update convertkit-subscribe.js

--- functions/convertkit-subscribe/convertkit-subscribe.js
+++ functions/convertkit-subscribe/convertkit-subscribe.js
@@ -14,6 +14,7 @@ exports.handler = async (event, context) => {
         method: 'post',
         body:    subscriber,
         headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        redirect: 'follow',
     })
     .then(res => res.json())
     .then(data => {
