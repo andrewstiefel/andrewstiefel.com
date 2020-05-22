eba36369d53a0f39169985ad8a97a9066f5926ab
Andrew S <andrew@andrewstiefel.com>
2020-05-21 22:59:50 -0700

    add then statement

--- functions/convertkit-subscribe/convertkit-subscribe.js
+++ functions/convertkit-subscribe/convertkit-subscribe.js
@@ -1,16 +1,6 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event, context) => {
-
-  // Redirect the page
-
-  const response = {
-    statusCode: 301,
-    headers: {
-      Location: "/almost",
-    },
-  };
-
   const email = event.queryStringParameters.email || "Oops, no email";
   const data = {
     api_key: "hQIOi5G6xVzZBQ0hRZTfKg",
@@ -21,7 +11,7 @@ exports.handler = async (event, context) => {
 
   // Subscribe an email
 
-  fetch("https://api.convertkit.com/v3/forms/1369284/subscribe", {
+  return fetch("https://api.convertkit.com/v3/forms/1369284/subscribe", {
     method: "post",
     body: subscriber,
     headers: { "Content-Type": "application/json; charset=utf-8" },
@@ -30,10 +20,13 @@ exports.handler = async (event, context) => {
     .then((data) => {
       console.log("Success:", data);
     })
+    .then(() => ({
+      statusCode: 301,
+      headers: {
+        Location: "/almost",
+      },
+    }))
     .catch((error) => {
       console.error("Error:", error);
     });
-
-   return response;
-
 };
