0794084aaabad8fa2f3e3ca8c21a09df889292da
Andrew S <andrew@andrewstiefel.com>
2022-02-01 17:19:32 -0800

    Fix console errors in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,38 +24,51 @@ var hamburger = document.querySelector(".navbar-burger");
     // Do something else, like open/close menu
 });
 
-// Copy url to share
-document.getElementById("copyURL").onclick = function() {
+// New Copy URL to share
+window.addEventListener('load', (event) => {
+  if(document.getElementById('copyURL')) {
+    copyURL.addEventListener('click', () => {
+      /* Get the text field */
+      var copyText = document.getElementById("postURL");
+      var copySuccess = document.getElementById("copyURL");
 
-  /* Get the text field */
-  var copyText = document.getElementById("postURL");
-  var copySuccess = document.getElementById("copyURL");
+      /* Select the text field */
+      copyText.select();
+      copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      /* Copy the text inside the text field */
+      navigator.clipboard.writeText(copyText.value);
 
-  /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
+      /* Alert the copied text */
+      copySuccess.classList.add('copied');
 
-  /* Alert the copied text */
-  copySuccess.classList.add('copied');
-
-  setTimeout(() => {
-    copySuccess.classList.remove('copied');
-  }, 2000);
-};
+      setTimeout(() => {
+        copySuccess.classList.remove('copied');
+      }, 2000);
+    });
+      } else {
+          console.log()
+      }
+    });
 
 // Subscribe to newsletter
 window.addEventListener('load', (event) => {
-    document.getElementById('subForm').addEventListener('submit', () => {
+  if(document.getElementById('subForm')) {
+    subForm.addEventListener('submit', () => {
         fathom.trackGoal('UFIQJWTI', 0);
     });
+  } else {
+    console.log()
+  }
 });
 
 // Subscribe to RSS
 window.addEventListener('load', (event) => {
-    document.getElementById('subRSS').addEventListener('click', () => {
-        fathom.trackGoal('YKQ6ZLVM', 0);
+  if(document.getElementById('subRSS')) {
+    subRSS.addEventListener('click', () => {
+      athom.trackGoal('YKQ6ZLVM', 0);
     });
+  } else {
+    console.log()
+  }
 });
