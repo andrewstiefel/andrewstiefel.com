017ff24061c3fd6260269a01d6b7c5d999169f39
Andrew S <andrew@andrewstiefel.com>
2022-06-26 17:22:29 -0700

    Initial build out of work for #31
    Create function, update form, and configure netlify.toml

---
+++ netlify/functions/subscribe/subscribe.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+const { CONVERTKIT_API_KEY } = process.env;
+
+exports.handler = async (event, context) => {
+  const { email } = JSON.parse(event.body);
+
+  const subscriber = {
+    api_key: CONVERTKIT_API_KEY,
+    email: email,
+  };
+
+  try {
+    await axios.post(
+      'https://api.convertkit.com/v3/forms/3384627/subscribe',
+      subscriber,
+    );
+    return {
+      statusCode: 200,
+      body: 'Email subscribed',
+    };
+  } catch (err) {
+    return {
+      statusCode: err.code,
+      body: JSON.stringify({ msg: err.message }),
+    };
+  }
+};
