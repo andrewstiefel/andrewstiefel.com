042570dae39184116edc7e67fef11697129f732b
Andrew S <andrew@andrewstiefel.com>
2022-08-25 21:32:21 -0700

    Substitute from StackOverflow

--- netlify/functions/subscribe-3384627.js
+++ netlify/functions/subscribe-3384627.js
@@ -5,7 +5,6 @@ exports.handler = async (event, context) => {
     const email = event.queryStringParameters.email || "No email";
     console.log(`Received a submission: ${email}`)
 
-    /*
     const response = await fetch(
         'https://api.convertkit.com/v3/forms/3384627/subscribe', {
             method: 'POST',
@@ -23,21 +22,5 @@ exports.handler = async (event, context) => {
     return {
         statusCode: response.status,
         body: responseText,
-    }
-*/
-    return fetch ('https://api.convertkit.com/v3/forms/3384627/subscribe', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            api_key: CONVERTKIT_API_KEY,
-            email: email
-        }),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(`Submitted to ConvertKit: ${data}`);
-    })
-    .catch((error) => ({ statusCode: 422, body: String(error) }))
+    };
 }
