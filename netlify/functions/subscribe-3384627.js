bc55d2b31f9e0400d26cf1dc665682b8c4749981
Andrew S <andrew@andrewstiefel.com>
2022-08-25 20:52:05 -0700

    Change to await fetch

--- netlify/functions/subscribe-3384627.js
+++ netlify/functions/subscribe-3384627.js
@@ -4,6 +4,31 @@ const { CONVERTKIT_API_KEY } = process.env;
 exports.handler = async (event, context) => {
     const email = event.queryStringParameters.email || "No email";
     console.log(`Received a submission: ${email}`)
+
+    const response = await fetch(
+        'https://api.convertkit.com/v3/forms/3384627/subscribe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                api_key: CONVERTKIT_API_KEY,
+                email: email
+             }),
+        }
+    );
+    let responseText = await response.text();
+    console.log('response:', responseText);
+    return {
+        statusCode: response.status,
+        body: responseText,
+    };
+
+
+/*
+exports.handler = async (event, context) => {
+    const email = event.queryStringParameters.email || "No email";
+    console.log(`Received a submission: ${email}`)
     return fetch ('https://api.convertkit.com/v3/forms/3384627/subscribe', {
         method: 'POST',
         headers: {
@@ -20,3 +45,4 @@ exports.handler = async (event, context) => {
       })
     .catch (error => ({ statusCode: 422, body: String(error) }))
   }
+  */
