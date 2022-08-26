e75a78c406e89931a17b4df9a82f4935de7f584e
Andrew S <andrew@andrewstiefel.com>
2022-08-25 18:12:45 -0700

    Edit subscribe.js function

--- netlify/functions/subscribe/subscribe.js
+++ netlify/functions/subscribe/subscribe.js
@@ -3,25 +3,24 @@ const fetch = require("node-fetch");
 const { CONVERTKIT_API_KEY } = process.env;
 
 exports.handler = async (event, context) => {
-    const { EMAIL } = JSON.parse(event.body).payload
+    const { EMAIL } = JSON.parse(event.body).payload.email
     console.log(`Received a submission: ${EMAIL}`)
     
     const { SUBSCRIBER } = {
         api_key: CONVERTKIT_API_KEY,
-        email: EMAIL
+        email: EMAIL,
       };
 
-    try {
-        const response = await fetch('https://api.convertkit.com/v3/forms/3384627/subscribe', {
+    return fetch('https://api.convertkit.com/v3/forms/3384627/subscribe', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8',
             },
             body: JSON.stringify( SUBSCRIBER ),
-        })
-    const DATA = await response.json()
-    console.log(`Submitted to ConvertKit:\n ${DATA}`)
-    } catch (error) {
-        return { statusCode: 422, body: String(error)}
-    }
+         })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Submitted to ConvertKit:\n ${data')
+            })
+            .catch (error => ({ statusCode: 422, body: String(error) }))
 }
