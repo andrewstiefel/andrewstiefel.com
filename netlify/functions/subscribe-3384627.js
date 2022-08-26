2fe71a04aab3360665746713d619d36ce9056bde
Andrew S <andrew@andrewstiefel.com>
2022-08-25 19:35:02 -0700

    Remove POST action

--- netlify/functions/subscribe-3384627.js
+++ netlify/functions/subscribe-3384627.js
@@ -1,6 +1,3 @@
-const fetch = require("node-fetch");
-// const { CONVERTKIT_API_KEY } = process.env;
-
 exports.handler = async (event, context) => {
     const email = event.queryStringParameters.email || "No email";
   
@@ -9,25 +6,3 @@ exports.handler = async (event, context) => {
       body: `Hello, ${email}`,
     };
   };
-
-/*
-exports.handler = async (event, context) => {
-    const email = JSON.parse(event.body).payload.email
-    console.log(`Received a submission: ${email}`)
-    return fetch('https://api.convertkit.com/v3/forms/3384627/subscribe', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                api_key: CONVERTKIT_API_KEY,
-                email: email
-            }),
-         })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Submitted to ConvertKit:\n ${data}')
-            })
-            .catch (error => ({ statusCode: 422, body: String(error) }))
-}
-*/
