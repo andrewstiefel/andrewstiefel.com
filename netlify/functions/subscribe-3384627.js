ec4131e8743ca91d43147a83a7038e3dc7cc8410
Andrew S <andrew@andrewstiefel.com>
2022-08-25 19:45:31 -0700

    Here goes something

--- netlify/functions/subscribe-3384627.js
+++ netlify/functions/subscribe-3384627.js
@@ -1,8 +1,23 @@
+const fetch = require("node-fetch");
+const { CONVERTKIT_API_KEY } = process.env;
+
 exports.handler = async (event, context) => {
     const email = event.queryStringParameters.email || "No email";
-  
-    return {
-      statusCode: 200,
-      body: `Hello, ${email}`,
-    };
-  };
+    console.log(`Received a submission: ${email}`)
+    return fetch ('https://api.convertkit.com/v3/forms/3384627/subscribe', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            api_key: CONVERTKIT_API_KEY,
+            email: email
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Submitted to ConvertKit:\n ${data}')
+    })
+    .catch (error => ({ statusCode: 422, body: String(error) }))
+
+  }
