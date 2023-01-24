ebdd745c7e41f22a75cdcd760c1f1cc6795d69b2
Andrew S <andrew@andrewstiefel.com>
2023-01-23 16:59:10 -0800

    Create new dark mode toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,49 @@ button.addEventListener('click', () => {
   body.classList.toggle('overflow-hidden');
 });
 
+// Toggle document theme
+let html = document.getElementById('html-tag');
+let themeText = document.querySelector(".theme-text");
+
+document.querySelector('#theme-toggle').addEventListener('click', () => {
+  if (!html.classList.contains('dark') && !html.classList.contains('light')) {
+    html.classList.remove('light');
+    html.classList.add('theme-dark', 'dark');
+    themeText.innerText = "Dark theme";
+
+    // store the value whenever the user explicitly chooses dark mode
+    localStorage.setItem('theme', 'dark');
+    
+  } else if (html.classList.contains('dark')) {
+    html.classList.remove('theme-dark', 'dark');
+    html.classList.add('light');
+    themeText.innerText = "Light theme";
+
+    // store the value whenever the user explicitly chooses light mode
+    localStorage.setItem('theme', 'light');
+
+  } else {
+    html.classList.remove('theme-dark', 'dark', 'light');
+    themeText.innerText = "System theme";
+
+    // remove the stored value when the user chooses to respect the OS prefernce
+    localStorage.removeItem('theme'); 
+  }
+});
+
+// Evaluate if we need to update the theme toggle text
+window.addEventListener('load', (event) => {
+  if (localStorage.theme === 'dark') {
+    themeText.innerText = "Dark theme";
+  }
+  else if (localStorage.theme === 'light') {
+    themeText.innerText = "Light theme";
+  }
+  else {
+    console.log()
+  }
+});
+
 // Subscribe to RSS
 window.addEventListener('load', (event) => {
   if(document.getElementById('subRSS')) {
