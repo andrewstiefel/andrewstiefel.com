7a6b8b6bc96af373b02d8a466ba0da0ebc16d912
Andrew Stiefel <andrew@andrewstiefel.com>
2024-03-29 16:24:07 +0000

    Update function

--- netlify/functions/submission-created.mjs
+++ netlify/functions/submission-created.mjs
@@ -1,21 +1,21 @@
 import fetch from 'node-fetch';
-import { createClient } from '@1password/sdk';
+import { createClient } from "@1password/sdk";
 
 const { OP_SERVICE_ACCOUNT_TOKEN } = process.env;
 
-export default async (event) => {
-  const submission = JSON.parse(event.body).payload.data
-  const email = submission.email
-  const tag = submission.tag
-  console.log(`Received a submission: ${email}, ${tag}`)
+export default async (request) => {
+  const body = await request.json();
+  const email = body.payload.email;
+  const tag = body.payload.tag;
+  console.log(`Received a submission: ${email}, ${tag}`);
 
   const client = await createClient({
     auth: OP_SERVICE_ACCOUNT_TOKEN,
-    integrationName: "andrewstiefel.com",
+    integrationName: "Netlify and 1Password Demo",
     integrationVersion: "v1.0.0",
   });
   
-  const secret = await client.secrets.resolve("op://website/buttondown-api/credential");
+  const secret = await client.secrets.resolve("op://sdk-beta/api-key/credential");
 
   const response = await fetch( 'https://api.buttondown.email/v1/subscribers', {
     method: 'POST',
@@ -26,15 +26,14 @@ export default async (event) => {
     body: JSON.stringify({ 
       email: email,
       tags: [tag]
-      }),
-    }
-  );
+     }),
+  });
 
   let responseText = await response.text();
-  console.log('Response:', responseText);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({})
-  };
+  console.log('Received response:', responseText);
+    
+  return new Response({
+    statusCode: 302,
+    Location: '/success/'
+  })
 };
